8133f71e857a6ba9c0a8811258c759355298107e
Abduhalim05 <abduhalimabdulkhamidov@gmail.com>
2022-05-28 15:53:03 +0500

    admin prapared & home crud

---
+++ resources/views/admin/home/index.blade.php
@@ -0,0 +1,91 @@
+@extends('layouts.admin')
+
+@section('css')
+    <!-- DataTables -->
+    <link rel="stylesheet" href="/assets/bundles/datatables/datatables.min.css">
+    <link rel="stylesheet" href="/assets/bundles/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
+
+@endsection
+
+@section('content')
+
+<div class="row">
+    <div class="col-12">
+      <div class="card">
+
+            <div class="card-header ">
+                <a class="btn btn-primary " href="{{ route('admin.homes.create')}}">Yaratish</a>
+            </div>
+
+        <div class="card-body">
+            <h5 align="center">Bosh menyu ma'lumotlar jadvali</h5>
+            @if (Session::has('success'))
+                <div class="alert alert-success alert-dismissible show fade">
+                    <div class="alert-body">
+                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                            <span>×</span>
+                        </button>
+                        <h5><i class="icon fas fa-check"></i></h5>
+                        {{session('success')}}
+                    </div>
+                </div>
+            @endif
+            @if (Session::has('warning'))
+                <div class="alert alert-danger alert-dismissible show fade">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true"> <span>×</span> </button>
+                    <h5><i class="icon fas fa-ban"></i> </h5>
+                    {{session('warning')}}
+                </div>
+            @endif
+          <div class="table-responsive">
+            <table class="table table-striped" id="table-1">
+              <thead>
+                <tr>
+                    <th class="text-center"> # </th>
+                    <th>Sarlovha(UZ)</th>
+                    <th>Tavsif(UZ)</th>
+                    <th>Rasmi</th>
+                    <th>Amallar</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($items as $item)
+                <tr class="odd">
+                    <td>{{$loop->iteration}}</td>
+                    <td >{{ $item->translate('uz')->title }}</td>
+                    <td >{{ $item->translate('uz')->description }}</td>
+
+                    <td class=""><img src="/admin/images/homes/{{$item->image}}" width="100px" alt="" srcset=""></td>
+
+                    <td class="d-flex justify-content-center ">
+                        <a class="btn btn-primary  " href="{{route('admin.homes.show', $item->id)}}">
+                            <i class="fas fa-eye"></i>
+                        </a>
+                        <a class="btn btn-info " href="{{route('admin.homes.edit', $item->id)}}">
+                            <i class="fas fa-pencil-alt"></i>
+                        </a>
+                        <form action="{{route('admin.homes.destroy', $item->id)}}" method="item">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">
+                                <i class="fas fa-trash"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+               @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+    <script src="/assets/bundles/datatables/datatables.min.js"></script>
+    <script src="/assets/bundles/datatables/DataTables-1.10.16/js/dataTables.bootstrap4.min.js"></script>
+    <script src="/assets/bundles/jquery-ui/jquery-ui.min.js"></script>
+    <script src="/assets/js/page/datatables.js"></script>
+@endsection
---
+++ resources/views/admin/home/show.blade.php
@@ -0,0 +1,174 @@
+@extends('layouts.admin')
+
+@section('css')
+        <link rel="stylesheet" href="/assets/bundles/summernote/summernote-bs4.css">
+        <link rel="stylesheet" href="/assets/bundles/codemirror/lib/codemirror.css">
+        <link rel="stylesheet" href="/assets/bundles/codemirror/theme/duotone-dark.css">
+    <style>
+
+        .bg-aqua-active{
+
+            background-color: #6777ef;
+            border-color: transparent !important;
+            color: #fff !important;
+
+        }
+
+
+    </style>
+@endsection
+
+@section('content')
+<section class="section">
+    <div class="section-body">
+        <div class="row">
+            <div class="col-12">
+                <div class="card"> <br>
+                    <div class="row mb-2">
+                        <div class="card-header col-sm-6">
+
+                            <div class=" d-flex justify-content-center">
+
+                                <a href="{{ route('admin.homes.edit', $item->id) }}"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square" aria-hidden="true"></i> Tahrirlash</button></a> &nbsp;
+                                <a href="{{ route('admin.homes.index') }}"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Ortga</button></a> &nbsp;
+
+                                <form action="{{route('admin.homes.destroy', $item->id)}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button  style="display: inline" type="submit" class="btn btn-danger btn-sm  ">
+                                        <i class="fas fa-trash"  aria-hidden="true"></i>
+                                        O'chirish
+                                    </button>
+                                </form>
+
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <ul class="nav nav-tabs float-sm-right " >
+                                <li class="nav-item">
+                                    <a class="nav-link " href="#" id="ru-link">Ru</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link bg-aqua-active" href="#" id="uz-link">Uzb</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#" id="en-link">EN</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <hr>
+
+                        <div class=" d-flex justify-content-center">
+                            <div class="box-header"><h5> Sarlovha</h5></div><br>
+                        </div>
+                        <div class="tab-content">
+                            <div class="tab-pane active uz-form">
+                                <h6> {{$item->translate('uz')->title}} </h6>
+                            </div>
+                            <div class="tab-pane active en-form d-none">
+                                <h6> {{$item->translate('en')->title}} </h6>
+                            </div>
+                            <div class="tab-pane active ru-form d-none">
+                                <h6> {{$item->translate('ru')->title}} </h6>
+                            </div>
+                        </div> <hr>
+
+                        <div class=" d-flex justify-content-center">
+                            <div class="box-header"><h5> Tavsif</h5></div><br>
+                        </div>
+                        <div class="tab-content">
+                            <div class="tab-pane active uz-form">
+                                <h6> {{$item->translate('uz')->description}} </h6>
+                            </div>
+                            <div class="tab-pane active en-form d-none">
+                                <h6> {{$item->translate('en')->description}} </h6>
+                            </div>
+                            <div class="tab-pane active ru-form d-none">
+                                <h6> {{$item->translate('ru')->description}} </h6>
+                            </div>
+                        </div> <hr>
+
+                        <div class=" d-flex justify-content-center">
+                            <div class="box-header"><h5> Rasm</h5></div><br>
+                        </div>
+                        <div class="tab-content">
+                            <div class="tab-pane active">
+                                <img src="/admin/images/homes/{{$item->image}}" width="15%" height="10%">
+                            </div>
+                        </div> <hr>
+
+                        <div class=" d-flex justify-content-center">
+                            <div class="box-header"><h5> Meta sarlovha (title) </h5></div><br>
+                        </div>
+                        <div class="tab-content">
+                            <div class="tab-pane active">
+                                <h6>  {{$item->meta_title}}  </h6>
+                            </div>
+                        </div> <hr>
+
+                        <div class=" d-flex justify-content-center">
+                            <div class="box-header"><h5> Meta tavsif (description)</h5></div><br>
+                        </div>
+                        <div class="tab-content">
+                            <div class="tab-pane active">
+                                <h6>  {{$item->meta_description}}  </h6>
+                            </div>
+                        </div> <hr>
+
+                        <div class=" d-flex justify-content-center">
+                            <div class="box-header"><h5> Meta kalitso'z (keywords) </h5></div><br>
+                        </div>
+                        <div class="tab-content">
+                            <div class="tab-pane active">
+                                <h6>  {{$item->meta_keywords}}  </h6>
+                            </div>
+                        </div> <hr>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
+
+@section('scripts')
+    <script>
+        var $uzForm = $('.uz-form');
+        var $ozForm = $('.en-form');
+        var $ruForm = $('.ru-form');
+        var $uzLink = $('#uz-link');
+        var $ruLink = $('#ru-link');
+        var $ozLink = $('#en-link');
+
+
+        $uzLink.click(function() {
+            $uzLink.addClass('bg-aqua-active');
+            $uzForm.removeClass('d-none');
+            $ruLink.removeClass('bg-aqua-active');
+            $ruForm.addClass('d-none');
+            $ozLink.removeClass('bg-aqua-active');
+            $ozForm.addClass('d-none');
+
+        });
+
+        $ruLink.click(function() {
+            $ruLink.addClass('bg-aqua-active');
+            $ruForm.removeClass('d-none');
+            $uzLink.removeClass('bg-aqua-active');
+            $uzForm.addClass('d-none');
+            $ozLink.removeClass('bg-aqua-active');
+            $ozForm.addClass('d-none');
+        });
+        $ozLink.click(function() {
+            $ozLink.addClass('bg-aqua-active');
+            $ozForm.removeClass('d-none');
+            $uzLink.removeClass('bg-aqua-active');
+            $uzForm.addClass('d-none');
+            $ruLink.removeClass('bg-aqua-active');
+            $ruForm.addClass('d-none');
+        });
+    </script>
+@endsection
