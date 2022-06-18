058f8995486cfa7e4a7ffd33c05ca1273aa8f799
Abdullayev19 <abdullayev09me@gmail.com>
2022-06-18 16:29:33 +0500

    status

---
+++ app/Http/Resources/StatusNewsResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StatusNewsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $id=$this->likes->all();
+       
+        $likes = count($id);
+        
+        return [
+
+            'id'=>$this->id,
+            'created_at'=>$this->created_at->format('d/m/Y'), 
+            'sport_category'=>$this->sport_category_id,
+            'status'=>$this->status,
+            'special'=>$this->special,
+            'likes' => $likes,
+            'image'=>$this->image,
+            'title_uz'=>$this->translate('uz')->title,
+            'title_ru'=>$this->translate('ru')->title,
+            'title_en'=>$this->translate('en')->title,
+            'description_uz'=>strip_tags($this->translate('uz')->description),
+            'description_ru'=>strip_tags($this->translate('ru')->description),
+            'description_en'=>strip_tags($this->translate('en')->description),
+        ];
+    }
+}
